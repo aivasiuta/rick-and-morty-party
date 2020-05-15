ec125829f26c2a1361aca1263ae245877fdb5478
alexey <ivasyutaalexey@gmail.com>
2020-05-15 23:56:57 +0300

    fix eslint errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { App } from './App'
 import { ApolloProvider } from '@apollo/react-hooks'
 
+import { App } from './App'
 import { client } from './apollo'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={ client }>
+    <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
